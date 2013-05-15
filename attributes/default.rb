955e27ab2be48a3c8ab84879cf49e6adb9b038d0
Seth Kingry <s.kingry@modcloth.com>
2013-05-15 16:46:07 +0000

    Removing the stuff that has us setting the private address and instead relying on a Gem

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,4 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-default[:hostname][:ipaddress]            = `ifconfig -a | egrep '192.168\|10.[0-9]\|172.16' | grep inet | awk '{print $2}'`.strip.to_s
 
