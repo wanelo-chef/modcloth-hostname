30ca6f8c559a9610f49846fb72b4303dc30cbb5e
Seth Kingry <s.kingry@modcloth.com>
2013-05-17 21:44:30 +0000

    Added support for centos

---
+++ recipes/centos.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: hostname
+# Recipe:: ubuntu
+#
+# Copyright ModCloth, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "set hostname to node name" do
+  command "hostname #{node.name}"
+end
+
+execute "Setting Hostname" do
+  command "sed -i -e 's/HOSTNAME\=localhost.localdomain/HOSTNAME\=#{node.name}/g' /etc/sysconfig/network"
+end
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,8 @@
 case node["platform"]
 when 'smartos'
   include_recipe "hostname::smartos"
+when 'centos'
+  include_recipe "hostname::centos"
 else 'ubuntu'
   include_recipe "hostname::ubuntu"
 end
